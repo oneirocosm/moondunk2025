bac823f63b8dc40771e849958b98b372ff488970
Sylvia Crowe <software@oneirocosm.com>
2025-08-28 14:29:54 -0700

    feat: add a dashboard to display donations

--- src/browser/dashboard/example.tsx
+++ src/browser/dashboard/example.tsx
@@ -9,19 +9,52 @@ import { render } from '../render';
 const App = () => {
   const [queuedDonations, _] = useReplicant<Array<Donation>>('queueddonations');
 
-
   return (
     <DashboardThemeProvider>
       <img src={exampleImage} />
-      {queuedDonations?.map((donation: Donation) => <div style={{
+      <div style={{
         display: "flex",
-        flexDirection: "column",
-        height: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "400px",
+        fontWeight: 600,
       }}>
-        <span>{donation.donor_name}</span>
-        <span>{donation.amountDisplay}</span>
-      </div>)}
-      <span>asdfs</span>
+        <span style={{
+
+        }}>Donor</span>
+        <span style={{
+          width: "80px",
+          minWidth: "80px",
+        }}>Donation</span>
+        <span style={{
+          width: "80px",
+          minWidth: "80px",
+        }}>Time</span>
+      </div>
+      {queuedDonations?.map((donation: Donation) => {
+
+        return <div key={`donation-row-${donation.id}`} style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "400px",
+      }}>
+        <span style={{
+          textOverflow: "ellipsis",
+          overflowX: "hidden",
+          whiteSpace: "nowrap",
+          flexGrow: 1,
+        }}>{donation.donor_name}</span>
+        <span style={{
+          minWidth: "80px",
+          width: "80px",
+        }}>${donation.amountDisplay.toFixed(2)}</span>
+        <span style={{
+          minWidth: "80px",
+          width: "80px",
+        }}>${donation.amountDisplay.toFixed(2)}</span>
+      </div>
+      })}
     </DashboardThemeProvider>
   );
 };
